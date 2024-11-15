afc51641a9bf30e20aa2302f0d02cfdaa94aa554
RoseSecurity <michaeljrosenfeld22@gmail.com>
2024-11-15 15:44:27 -0500

    feat: add kuzco list models command

---
+++ cmd/list.go
@@ -0,0 +1,17 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// listCmd commands lists models
+var listCmd = &cobra.Command{
+	Use:                "list",
+	Short:              "Lists available Ollama models",
+	Long:               `Queries and lists available Ollama models for Kuzco use`,
+	FParseErrWhitelist: struct{ UnknownFlags bool }{UnknownFlags: false},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
---
+++ pkg/utils/list_models.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Model struct {
+	Name string `json:"name"`
+}
+
+// ListModels lists available models from Ollama
+func ListModels(addr string) ([]string, error) {
+	resp, err := http.Get(fmt.Sprintf("%s/api/tags", addr))
+	if err != nil {
+		return nil, fmt.Errorf("error fetching models from Ollama: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to retrieve models: status code %d", resp.StatusCode)
+	}
+
+	var result struct {
+		Models []Model `json:"models"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return nil, fmt.Errorf("error decoding response: %v", err)
+	}
+
+	var modelNames []string
+	for _, model := range result.Models {
+		modelNames = append(modelNames, model.Name)
+	}
+
+	return modelNames, nil
+}
